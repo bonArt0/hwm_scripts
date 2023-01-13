76f59c81bbc9eff7ad3b47aa388f658d0d183c1c
bonArt <bonArt.it@yandex.ru>
2023-01-14 02:00:41 +0300

    hwm_armory_framework 1.1.0

--- HWM_ArmoryUpgrade/scripts/hwm_armory_framework.user.js
+++ HWM_ArmoryUpgrade/scripts/hwm_armory_framework.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          HWM Armory Framework
 // @namespace     https://github.com/bonArt0/hwm_scripts
-// @version       1.0.1
+// @version       1.1.0
 // @description   Хелпер для других скриптов склада
 // @author        bonArt
 // @license       GPL-3.0-only
@@ -13,7 +13,13 @@
 // @supportURL    https://www.heroeswm.ru/sms-create.php?mailto_id=117282
 // ==/UserScript==
 
-if (isControlOn() && initFramework() !== undefined) {
+const FrameworkClassNames = {
+    ARTS_PLACE_FORM: 'afw_arts_place_form',
+    ARTS_PLACE_HEADER: 'afw_arts_place_header',
+    ARMORY_INFO: 'afw_armory_info',
+};
+
+if (isControlOn() && initFramework()) {
 }
 
 /**
@@ -24,56 +30,151 @@ function isControlOn() {
 }
 
 /**
- * @returns {HTMLTableRowElement[]|undefined}
+ * @returns {boolean}
  */
 function initFramework() {
-    const artsList = findArtsListBlock();
-    if (artsList === undefined) {
-        console.debug('something wrong');
-        return artsList;
+    return initArtPlaceBox() && initArmoryInfoBox();
+}
+
+/* <editor-fold desc="armory info box"> */
+
+/**
+ * @returns {boolean}
+ */
+function initArmoryInfoBox() {
+    const box = getArtPlaceForm()
+        ?.parentElement // td
+        ?.parentElement // tr
+        ?.parentElement // tbody
+        ?.parentElement // table#1
+        ?.previousSibling // table#0
+        ?.firstChild // tbody
+        ?.firstChild // tr
+        ?.firstChild; // td
+
+    if (!box || box.tagName !== 'TD') {
+        console.error('Something happen with game layout');
+        return false;
     }
 
-    artsList.map((value) => {
-        const id = value.getElementsByTagName('input').namedItem('inv_id').value;
-        value.classList.add('art_block', formatArtBlockClassName(id));
-    });
+    box.classList.add(FrameworkClassNames.ARMORY_INFO);
 
-    return artsList;
+    return true;
 }
 
 /**
- * @returns {HTMLTableRowElement[]|undefined}
+ * @returns {HTMLTableCellElement|null}
  */
-function findArtsListBlock() {
-    let currentBlock = Array.from(document.getElementsByTagName('body')).pop();
-    currentBlock = Array.from(currentBlock.children)[1];
-    if (!currentBlock) {
-        console.error('Block not found: center #1');
-        return undefined;
+function getArmoryInfoBox() {
+    const box = document.getElementsByClassName(FrameworkClassNames.ARMORY_INFO)?.item(0);
+    if (!box || box.tagName !== 'td') {
+        initArmoryInfoBox();
+        return document.getElementsByClassName(FrameworkClassNames.ARMORY_INFO)?.item(0) ?? null;
     }
-
-    currentBlock = Array.from(currentBlock.children).pop()
-    if (!currentBlock) {
-        console.error('Block not found: table #2');
-        return undefined;
-    }
-
-    currentBlock = Array.from(currentBlock.getElementsByTagName('table')).pop()
-    if (!currentBlock) {
-        console.error('Block not found: table #3');
-        return undefined;
-    }
-
-    currentBlock = Array.from(currentBlock.getElementsByTagName('tr'));
-    currentBlock.shift();
-    if (!currentBlock) {
-        console.error('Block not found: tr\'s');
-        return undefined;
-    }
-
-    return currentBlock;
+    return box;
 }
 
-function formatArtBlockClassName(id) {
-    return 'art_block_' + id;
+/**
+ * @returns {number}
+ */
+function getArmoryId() {
+    return +getArtPlaceForm()?.children?.item(0)?.value;
 }
+
+/**
+ * @returns {string}
+ */
+function getArtsPlaceSign() {
+    return getArtPlaceForm()?.children?.item(1)?.value + '';
+}
+
+/**
+ * @returns {string}
+ */
+function getCurrentCapacity() {
+    return  getArmoryInfoBox()?.innerHTML.match(/<b>(\d+)<\/b> из \d+/)[1];
+}
+
+/**
+ * @returns {string}
+ */
+function getTotalCapacity() {
+    return  getArmoryInfoBox()?.innerHTML.match(/<b>\d+<\/b> из (\d+)/)[1];
+}
+
+/* </editor-fold> */
+
+/* <editor-fold desc="arts place box"> */
+
+/**
+ * @returns {boolean}
+ */
+function initArtPlaceBox() {
+    const artsPlaceForm = document.getElementsByName('p_art_id')
+        ?.item(0) // select
+        ?.parentElement // td
+        ?.parentElement // tr
+        ?.parentElement // tbody
+        ?.parentElement // table
+        ?.parentElement;
+
+    if (!artsPlaceForm || artsPlaceForm.tagName !== 'FORM') {
+        console.error('Something happen with game layout');
+        return false;
+    }
+
+    artsPlaceForm.classList.add(FrameworkClassNames.ARTS_PLACE_FORM);
+
+    const artsPlaceHeader = artsPlaceForm
+        ?.parentElement // td
+        ?.parentElement // tr#1
+        ?.parentElement // tbody
+        ?.children.item(0) // tr#0
+        ?.children.item(0) // td#0
+        ?.children.item(0); // b
+
+    if (!artsPlaceHeader || artsPlaceHeader.tagName !== 'B') {
+        artsPlaceForm.classList.remove(FrameworkClassNames.ARTS_PLACE_FORM);
+        console.error('Something happen with game layout');
+        return false;
+    }
+
+    artsPlaceHeader?.classList.add(FrameworkClassNames.ARTS_PLACE_HEADER);
+
+    return true;
+}
+
+/**
+ * @returns {HTMLFormElement|null}
+ */
+function getArtPlaceForm() {
+    const form = document.getElementsByClassName(FrameworkClassNames.ARTS_PLACE_FORM)?.item(0);
+    if (!form || form.tagName !== 'FORM') {
+        initArtPlaceBox();
+        return document.getElementsByClassName(FrameworkClassNames.ARTS_PLACE_FORM)?.item(0) ?? null;
+    }
+    return form;
+}
+
+/* </editor-fold> */
+
+/*  <editor-fold desc="common"> */
+
+/**
+ * @param {string} name
+ * @param {string} innerHTML
+ * @returns {HTMLLabelElement}
+ */
+function buildCheckboxLabel(name, innerHTML) {
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = name;
+
+    const label = document.createElement('label');
+    label.append(checkbox);
+    label.append(innerHTML);
+
+    return label;
+}
+
+/* </editor-fold> */
