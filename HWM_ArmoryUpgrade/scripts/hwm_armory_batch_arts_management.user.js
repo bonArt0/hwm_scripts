e90165de0610bc921b4001c87bf619f2d6de4ac2
bonArt <bonArt.it@yandex.ru>
2023-01-21 18:03:45 +0300

    hwm_armory_batch_arts_management 1.1.3

--- HWM_ArmoryUpgrade/scripts/hwm_armory_batch_arts_management.user.js
+++ HWM_ArmoryUpgrade/scripts/hwm_armory_batch_arts_management.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name          HWM Armory Batch Arts Management
+// @name          hwm_armory_batch_arts_management
 // @namespace     https://github.com/bonArt0/hwm_scripts
-// @version       1.1.2
+// @version       1.1.3
 // @description   Движение артов пачкой
 // @author        bonArt
 // @license       GPL-3.0-only
